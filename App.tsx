97bdf4bf4ee9b93d0fddfd57890258e97a846a76
Walisson-Wander <srmks78@gmail.com>
2023-12-23 15:50:25 -0300

    Foi adicionado os lembretes junto com a biblioteca asyncStorage

--- App.tsx
+++ App.tsx
@@ -1,47 +1,143 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import React,{useEffect, useState} from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View , StyleSheet, TextInput, Text, TouchableOpacity, FlatList} from "react-native";
 
-const App = () => {
-  const [name, setName] = useState('');
-  const [DATA, setDATA] = useState<any[]>([]);
-
-  const handleAddName = () => {
-    if (name) {
-      setDATA([{ id: Date.now().toString(), name }, ...DATA]);
-      setName('');
+ const App=()=>{
+  const [DADOS,setDados]=useState<any[]>([]);
+  const [lembrete,setLembrete]=useState('');
+  const obterDadosSalvos=async()=>{
+    const chave = 'lembretes';
+    try{
+      const dadosJsonString = await AsyncStorage.getItem(chave);
+      if(dadosJsonString){
+        const dadosColetados=JSON.parse(dadosJsonString);
+        setDados(dadosColetados);
+      }
+    }catch(error){
+      console.error('Erro ao tentar coletar os dados ')
     }
-  };
+  }
+  useEffect(()=>{
+    obterDadosSalvos();
+    console.log('Dados adquiridos',DADOS);
+  },[]);
 
-  const handleDeleteName = (id:any) => {
-    const updatedData = DATA.filter(item => item.id !== id);
-    setDATA(updatedData);
-  };
+  const salvarDados=async(item:any)=>{
+    setDados(item)
+    try{
+      const chave ='lembretes';
+      const dadosJsonString=JSON.stringify(item);
+      await AsyncStorage.setItem(chave,dadosJsonString);
+    }catch(error){
+      console.error('erro ao tentar salvar os dados')
+    }
+  }
 
-  return (
-    <View style={{ flex: 1, padding: 20 }}>
-      <TextInput
-        placeholder="Digite um nome"
-        value={name}
-        onChangeText={text => setName(text)}
-      />
-      <TouchableOpacity onPress={handleAddName} style={{ marginTop: 10, padding: 10, backgroundColor: 'blue', alignItems: 'center' }}>
-        <Text style={{ color: 'white' }}>Adicionar</Text>
-      </TouchableOpacity>
+  const handleAddLembrete=()=>{
+    console.log('=========ADICIONANDO LEMBRETE========');
+    if(lembrete){
+      const newDADOS=[{id:Date.now().toString(),lembrete},...DADOS];
+      console.log("Lembretes",newDADOS);
+      setLembrete('');
+      salvarDados(newDADOS);
+    }
+  }
+  const handleExcluirLembrete=(id:any)=>{
+    console.log('xxxxxxxxx-EXCLUINDO LEMBRETE-xxxxxxxxxxxxx');
+    try{
+      const upadteDATA=DADOS.filter(item=>item.id !== id); 
+      salvarDados(upadteDATA);   
+    }catch(error){
+      console.error('Não foi possivel excluir lembrete')
+    }
+  }
+  return(
+    <View style={style.telaDeFundo}>
       <FlatList
-        data={DATA}
-        keyExtractor={(item:any) => item.id}
-        renderItem={({ item }) => (
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop: 10 }}>
-            <Text>{item.name}</Text>
-            <Text>{item.id}</Text>
-            <TouchableOpacity onPress={() => handleDeleteName(item.id)} style={{ padding: 5, backgroundColor: 'red', borderRadius: 5 }}>
-              <Text style={{ color: 'white' }}>Excluir</Text>
-            </TouchableOpacity>
+      data={DADOS}
+      keyExtractor={(item)=>item.id}
+      renderItem={({item})=>(
+        <TouchableOpacity
+        onPress={()=>handleExcluirLembrete(item.id)}
+        >
+          <View style={{ 
+            flexDirection: 'row', 
+            justifyContent: 'space-between', 
+            alignItems: 'center', 
+            marginTop: 10,
+            width:330,
+            backgroundColor:'#fff',
+            minHeight:60,
+            borderRadius:20,
+            padding:10 
+            }}>
+            <Text style={{
+              fontSize:16
+            }}>{item.lembrete}</Text>
           </View>
-        )}
+        </TouchableOpacity>
+        
+      )}
       />
+      <View style={style.inserirLembreteBox}>
+        <TextInput
+        onChangeText={Text=>setLembrete(Text)}
+        placeholder="Inserir Lembrete"
+        value={lembrete}
+        style={style.lembreteTextInput}
+        multiline={true}
+        />
+        <TouchableOpacity
+        onPress={handleAddLembrete}
+        style={style.CriarLembreteButtonBox}
+        >
+          <Text style={style.ciarLembretebuttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-};
+}
 
 export default App;
+
+const style = StyleSheet.create({
+  telaDeFundo:{
+    paddingTop:40,
+    paddingBottom:120,
+    width:'100%',
+    height:'100%',
+    backgroundColor:'#cce',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  inserirLembreteBox:{
+    position:'absolute',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    bottom:20
+  },
+  lembreteTextInput:{
+    elevation:10,
+    padding:20,
+    borderRadius:20,
+    backgroundColor:'#fff',
+    width:270,
+    minHeight:60,
+  },
+  CriarLembreteButtonBox:{
+    flexDirection:'column-reverse',
+    height:'100%',
+    alignItems:'center',
+  },
+  ciarLembretebuttonText:{
+    elevation:10,
+    backgroundColor:'#fff',
+    width:60,
+    height:60,
+    margin:10,
+    borderRadius:30,
+    fontSize:40,
+    textAlign:'center'
+  }
+
+});
